eb6d24cf1c9721000804de24e5cc15c8b16002b0
Finkton <saad.jmal@gmail.com>
2018-11-06 07:31:45 +0500

    Update app.js

--- app.js
+++ app.js
@@ -229,3 +229,28 @@ var c1 = document.getElementById("c1");
 console.log(toString(c1.style.height));
 
 myDoughnutChart.canvas.parentNode.style.height = '400px';
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI to notify the user they can add to home screen
+  addBtn.style.display = 'block';
+
+  addBtn.addEventListener('click', (e) => {
+    // hide our user interface that shows our A2HS button
+    addBtn.style.display = 'none';
+    // Show the prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        deferredPrompt = null;
+      });
+  });
+});
