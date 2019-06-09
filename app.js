281a8f0d50f17dff410ad82c9de77ef64b172ddd
Finkton <saad.jmal@gmail.com>
2019-06-08 20:47:36 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//____________________________PWA STUFF_________________________________________
 if ('serviceWorker' in navigator && 'PushManager' in window) {
   console.log('Service Worker and Push is supported');
 
@@ -56,4 +57,112 @@ window.addEventListener('appinstalled', (evt) => {
 if (window.matchMedia('(display-mode: standalone)').matches) {
   var btnAdd = document.getElementById("btnAdd");
   btnAdd.parentNode.removeChild(btnAdd);
+}
+
+//____________________________TF STUFF_________________________________________
+
+//Load, format and visualize the input data
+
+async function getData() {
+  const carsDataReq = await fetch('https://storage.googleapis.com/tfjs-tutorials/carsData.json');  
+  const carsData = await carsDataReq.json();  
+  const cleaned = carsData.map(car => ({
+    mpg: car.Miles_per_Gallon,
+    horsepower: car.Horsepower,
+  }))
+  .filter(car => (car.mpg != null && car.horsepower != null));
+  
+  return cleaned;
+}
+
+async function run() {
+  // Load and plot the original input data that we are going to train on.
+  const data = await getData();
+  const values = data.map(d => ({
+    x: d.horsepower,
+    y: d.mpg,
+  }));
+
+  tfvis.render.scatterplot(
+    {name: 'Horsepower v MPG'},
+    {values}, 
+    {
+      xLabel: 'Horsepower',
+      yLabel: 'MPG',
+      height: 300
+    }
+  );
+
+  // More code will be added below
+  const tensorData = convertToTensor(data);
+  const {inputs, labels} = tensorData;
+
+  // Train the model  
+  await trainModel(model, inputs, labels);
+  console.log('Done Training');
+}
+
+document.addEventListener('DOMContentLoaded', run);
+
+
+// Define the model architecture
+
+function createModel() {
+  // Create a sequential model
+  const model = tf.sequential(); 
+  
+  // Add a single hidden layer
+  model.add(tf.layers.dense({inputShape: [1], units: 1, useBias: true}));
+  
+  // Add an output layer
+  model.add(tf.layers.dense({units: 1, useBias: true}));
+
+  return model;
+}
+
+const model = createModel();  
+tfvis.show.modelSummary({name: 'Model Summary'}, model);
+
+// Prepare the data for training
+
+/**
+ * Convert the input data to tensors that we can use for machine 
+ * learning. We will also do the important best practices of _shuffling_
+ * the data and _normalizing_ the data
+ * MPG on the y-axis.
+ */
+function convertToTensor(data) {
+  // Wrapping these calculations in a tidy will dispose any 
+  // intermediate tensors.
+  
+  return tf.tidy(() => {
+    // Step 1. Shuffle the data    
+    tf.util.shuffle(data);
+
+    // Step 2. Convert data to Tensor
+    const inputs = data.map(d => d.horsepower)
+    const labels = data.map(d => d.mpg);
+
+    const inputTensor = tf.tensor2d(inputs, [inputs.length, 1]);
+    const labelTensor = tf.tensor2d(labels, [labels.length, 1]);
+
+    //Step 3. Normalize the data to the range 0 - 1 using min-max scaling
+    const inputMax = inputTensor.max();
+    const inputMin = inputTensor.min();  
+    const labelMax = labelTensor.max();
+    const labelMin = labelTensor.min();
+
+    const normalizedInputs = inputTensor.sub(inputMin).div(inputMax.sub(inputMin));
+    const normalizedLabels = labelTensor.sub(labelMin).div(labelMax.sub(labelMin));
+
+    return {
+      inputs: normalizedInputs,
+      labels: normalizedLabels,
+      // Return the min/max bounds so we can use them later.
+      inputMax,
+      inputMin,
+      labelMax,
+      labelMin,
+    }
+  });  
 }
